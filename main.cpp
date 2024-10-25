220ae299f53e7dd52decc1bc1577ae95abbde8d7
nico-fico <nikoloban04@gmail.com>
2024-10-25 15:57:26 +0200

    minor fix

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
 #include "particle.hpp"
 
-int main()
+void SetUp()
 {
-  Particle::AddParticleType(new ParticleType("pi+", 0.13957, 1));
-  Particle::AddParticleType(new ParticleType("pi-", 0.13957, -1));
-  Particle::AddParticleType(new ParticleType("K+", 0.49367, 1));
-  Particle::AddParticleType(new ParticleType("K-", 0.49367, -1));
+  gRANDOM->SetSeed();
   Particle::AddParticleType(new ParticleType("p+", 0.93827, 1));
   Particle::AddParticleType(new ParticleType("p-", 0.93827, -1));
+  Particle::AddParticleType(new ParticleType("K+", 0.49367, 1));
+  Particle::AddParticleType(new ParticleType("K-", 0.49367, -1));
+  Particle::AddParticleType(new ParticleType("pi+", 0.13957, 1));
+  Particle::AddParticleType(new ParticleType("pi-", 0.13957, -1));
   Particle::AddParticleType(new ResonanceType("K*", 0.89166, 0, 0.05));
+}
+
+int main()
+{
+  SetUp();
 
   Particle::PrintParticleTypes();
   Particle::ClearParticleTypes();
